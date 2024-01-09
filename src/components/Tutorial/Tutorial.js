848ee154b7ace047480fd560e5e5540a715e4010
Callum Bondy <callum.bondy@gmail.com>
2024-01-08 18:21:54 -0800

    more tutorial work

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 import './Tutorial.css'
 import stepOne from '../../assets/tut1gif-ezgif.com-crop.gif'
 import stepTwo from '../../assets/staticTutONE.png'
+import stepThree1 from '../../assets/tut3right.png'
+import stepThree2 from '../../assets/tut3wrong.png'
 
 export default function Tutorial(){
     return (
@@ -12,16 +14,31 @@ export default function Tutorial(){
                 Tutorial
             </div>
             <div className='tutorials'>
-            <div className='tutorialTwo'>
-                    <img className='tutorialTwoImage' src={stepTwo}/>
-                    <p>The word must start in the left most column and end on the indicated square.</p>
-                </div>
+                <div className='rowOne'>
+                    <div className='tutorialTwo'>
+                        <img className='tutorialTwoImage' src={stepTwo}/>
+                        <p>The word must start in the left most column and end on the indicated square.</p>
+                    </div>
                 
-                <div className='barrier'></div>
+                    <div className='barrier'></div>
 
-                <div className='tutorialOne'>
-                    <img className='tutorialOneGif' src={stepOne}/>
-                    <p>Drag tiles from the letter bank onto the board to form valid words.</p>
+                    <div className='tutorialOne'>
+                        <img className='tutorialOneGif' src={stepOne}/>
+                        <p>Drag tiles from the letter bank onto the board to form valid words.</p>
+                    </div>
+
+                </div>
+
+                <div className='rowOne'>
+                    <div className='tutorialThree'>
+                        <img className='tutorialThreeR' src={stepThree1}></img>
+                        <p>Construct your words so that each letter forms a continuous path.</p>
+                    </div>
+                    <div className='barrier'></div>
+                    <div className='tutorialThree'>
+                        <img className='tutorialThreeI' src={stepThree2}></img>
+                        <p>Although both words above are valid, the solution would not be correct.</p>
+                    </div>
                 </div>
             </div>
             <div className='buttons'>
